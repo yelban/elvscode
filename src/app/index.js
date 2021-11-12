4b854e9bb081f2b1774e4bc43c70b4c5975e2f89
Yelban <yelban@gmail.com>
2021-11-12 13:22:33 +0800

    parcel with express hot reload

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,12 @@
 console.log("index.js");
+
+const express = require("express");
+// var server = express.createServer();
+// express.createServer()  is deprecated.
+const server = express(); // better instead
+const path = require("path");
+
+// app.use(express.static(__dirname)); // Current directory is root
+server.use(express.static(path.join(__dirname, "../../dist")));
+
+server.listen(3000);
